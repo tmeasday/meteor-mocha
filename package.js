9518986a1f5a82321f7dc8a699b4843ced2e4048
Tom Coleman <tom@thesnail.org>
2016-03-07 13:25:25 +1100

    Doing another release

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 // Using the "wrapper package" version format
 Package.describe({
-  name: "practicalmeteor:mocha",
+  name: "avital:mocha",
   summary: "Write package tests with mocha and run them in the browser or from the command line with spacejam.",
   git: "https://github.com/practicalmeteor/meteor-mocha.git",
-  version: '2.1.0_6',
+  version: '2.1.0_9',
   testOnly: true
 });
 
